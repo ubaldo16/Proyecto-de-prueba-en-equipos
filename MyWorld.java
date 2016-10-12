c69bff32052cd5d572a6e0e926ebf09f4d23992b
ubaldo16 <o.j.e.u1695@gmail.com>
2016-10-12 10:50:51 -0500

    Proyecto de prueba en equipos
    se creo el pryecto de prueba

---
+++ MyWorld.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import greenfoot.*;
+import java.awt.Color;
+
+/**
+ * 
+ */
+public class MyWorld extends World
+{
+
+    /**
+     * Constructor for objects of class MyWorld.
+     */
+    public MyWorld()
+    {
+        super(600, 400, 1);
+    }
+}
